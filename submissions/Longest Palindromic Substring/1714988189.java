b9336668e52f9b331fe299d4efea33e08137065b
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-07-29 00:35:44 +0530

    Add submission 1714988189

---
+++ submissions/Longest Palindromic Substring/1714988189.java
@@ -0,0 +1,41 @@
+# Title: Longest Palindromic Substring
+# Submission ID: 1714988189
+# Status: Accepted
+# Date: July 29, 2025 at 12:35:38 AM GMT+5:30
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1)
+            return "";
+
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandFromMiddle(s, i, i);       // odd length
+            int len2 = expandFromMiddle(s, i, i + 1);   // even length
+            int len = Math.max(len1, len2);
+            
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    private int expandFromMiddle(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;  // length of palindrome
+    }
+
+    // Sample test
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.longestPalindrome("babad")); // "bab" or "aba"
+        System.out.println(sol.longestPalindrome("cbbd"));  // "bb"
+    }
+}
