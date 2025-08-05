304654e6c88564f698b536ee4d6bcf8dd7d1f814
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-08-06 00:55:35 +0530

    Add submission 1724863671

---
+++ submissions/Fruits Into Baskets II/1724863671.java
@@ -0,0 +1,28 @@
+# Title: Fruits Into Baskets II
+# Submission ID: 1724863671
+# Status: Accepted
+# Date: August 6, 2025 at 12:55:27 AM GMT+5:30
+
+public class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        boolean[] used = new boolean[n];
+        int unplaced = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean placed = false;
+            for (int j = 0; j < n; j++) {
+                if (!used[j] && baskets[j] >= fruits[i]) {
+                    used[j] = true;
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) {
+                unplaced++;
+            }
+        }
+
+        return unplaced;
+    }
+}
