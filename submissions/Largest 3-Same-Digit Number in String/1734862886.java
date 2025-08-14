3e4e540aed1b54a1650301cf46072f5d50955740
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-08-14 16:55:07 +0530

    Add submission 1734862886

---
+++ submissions/Largest 3-Same-Digit Number in String/1734862886.java
@@ -0,0 +1,32 @@
+# Title: Largest 3-Same-Digit Number in String
+# Submission ID: 1734862886
+# Status: Accepted
+# Date: August 14, 2025 at 04:55:01 PM GMT+5:30
+
+class Solution {
+    private List<String> sameDigitNumbers = List.of("999", "888", "777", "666", "555", "444", "333", "222", "111", "000");
+
+    // Check whether the 'num' string contains the 'sameDigitNumber' string or not.
+    private boolean contains(String sameDigitNumber, String num) {
+        for (int index = 0; index <= num.length() - 3; ++index) {
+            if (num.charAt(index) == sameDigitNumber.charAt(0) &&
+                num.charAt(index + 1) == sameDigitNumber.charAt(1) &&
+                num.charAt(index + 2) == sameDigitNumber.charAt(2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String largestGoodInteger(String num) {
+        // Iterate on all 'sameDigitNumbers' and check if the string 'num' contains it.
+        for (String sameDigitNumber : sameDigitNumbers) {
+            if (contains(sameDigitNumber, num)) {
+                // Return the current 'sameDigitNumbers'.
+                return sameDigitNumber;
+            }
+        }
+        // No 3 consecutive same digits are present in the string 'num'.
+        return "";
+    }
+}
