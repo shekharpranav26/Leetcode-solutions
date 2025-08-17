a3fbb3e13c7e416cce991009a4c22ad84d941f43
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-08-18 00:01:03 +0530

    Add submission 1738873451

---
+++ submissions/New 21 Game/1738873451.java
@@ -0,0 +1,23 @@
+# Title: New 21 Game
+# Submission ID: 1738873451
+# Status: Time Limit Exceeded
+# Date: August 18, 2025 at 12:00:57 AM GMT+5:30
+
+class Solution {
+    public double new21Game(int n, int k, int maxPts) {
+        double dp[] = new double[n + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= maxPts; j++) {
+                if (i - j >= 0 && i - j < k) {
+                    dp[i] += dp[i - j] / maxPts;
+                }
+            }
+        }
+        double ans = 0;
+        for (int i = k; i <= n; i++) {
+            ans += dp[i];
+        }
+        return ans;
+    }
+}
