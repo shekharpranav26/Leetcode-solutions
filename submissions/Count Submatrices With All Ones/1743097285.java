0e0cad22d2e4459c0adeea90e71adafeb864a18b
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-08-21 16:22:06 +0530

    Add submission 1743097285

---
+++ submissions/Count Submatrices With All Ones/1743097285.java
@@ -0,0 +1,33 @@
+# Title: Count Submatrices With All Ones
+# Submission ID: 1743097285
+# Status: Accepted
+# Date: August 21, 2025 at 04:21:59 PM GMT+5:30
+
+class Solution {
+
+    public int numSubmat(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int res = 0;
+        int[][] row = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    row[i][j] = mat[i][j];
+                } else {
+                    row[i][j] = mat[i][j] == 0 ? 0 : row[i][j - 1] + 1;
+                }
+                int cur = row[i][j];
+                for (int k = i; k >= 0; k--) {
+                    cur = Math.min(cur, row[k][j]);
+                    if (cur == 0) {
+                        break;
+                    }
+                    res += cur;
+                }
+            }
+        }
+        return res;
+    }
+}
