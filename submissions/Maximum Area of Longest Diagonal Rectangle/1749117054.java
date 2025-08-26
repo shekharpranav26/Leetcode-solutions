dd3f1b5c69fe023a950c48e85cd38d7b3523f421
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-08-26 20:14:58 +0530

    Add submission 1749117054

---
+++ submissions/Maximum Area of Longest Diagonal Rectangle/1749117054.java
@@ -0,0 +1,25 @@
+# Title: Maximum Area of Longest Diagonal Rectangle
+# Submission ID: 1749117054
+# Status: Accepted
+# Date: August 26, 2025 at 08:14:51 PM GMT+5:30
+
+class Solution {
+
+    public int areaOfMaxDiagonal(int[][] dimensions) {
+        int maxDiaSq = 0;
+        int maxArea = 0;
+        for (int[] dim : dimensions) {
+            int l = dim[0];
+            int w = dim[1];
+            int diaSq = l * l + w * w;
+            int area = l * w;
+            if (diaSq > maxDiaSq) {
+                maxDiaSq = diaSq;
+                maxArea = area;
+            } else if (diaSq == maxDiaSq) {
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+        return maxArea;
+    }
+}
