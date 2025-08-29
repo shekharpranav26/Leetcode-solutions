4697a55321eae8baa0e75933c641a7749d7330df
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-08-29 21:23:03 +0530

    Add submission 1752593722

---
+++ submissions/Alice and Bob Playing Flower Game/1752593722.java
@@ -0,0 +1,11 @@
+# Title: Alice and Bob Playing Flower Game
+# Submission ID: 1752593722
+# Status: Accepted
+# Date: August 29, 2025 at 09:22:56 PM GMT+5:30
+
+class Solution {
+
+    public long flowerGame(int n, int m) {
+        return ((long) m * n) / 2;
+    }
+}
