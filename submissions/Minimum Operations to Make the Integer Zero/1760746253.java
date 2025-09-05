85cd63aa867899d66c27cec653e4118f006748db
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-09-05 23:20:05 +0530

    Add submission 1760746253

---
+++ submissions/Minimum Operations to Make the Integer Zero/1760746253.java
@@ -0,0 +1,21 @@
+# Title: Minimum Operations to Make the Integer Zero
+# Submission ID: 1760746253
+# Status: Accepted
+# Date: September 5, 2025 at 11:19:58 PM GMT+5:30
+
+class Solution {
+
+    public int makeTheIntegerZero(int num1, int num2) {
+        int k = 1;
+        while (true) {
+            long x = num1 - (long) num2 * k;
+            if (x < k) {
+                return -1;
+            }
+            if (k >= Long.bitCount(x)) {
+                return k;
+            }
+            k++;
+        }
+    }
+}
