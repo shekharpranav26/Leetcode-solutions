37b7dbaf08325c5d665a6ebf3d0fc1c0b813962d
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-09-22 21:01:18 +0530

    Add submission 1779228976

---
+++ submissions/Count Elements With Maximum Frequency/1779228976.java
@@ -0,0 +1,29 @@
+# Title: Count Elements With Maximum Frequency
+# Submission ID: 1779228976
+# Status: Accepted
+# Date: September 22, 2025 at 09:01:11 PM GMT+5:30
+
+public class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        // Find the frequency of each element
+        Map<Integer, Integer> frequencies = new HashMap<>();
+        for (int num : nums) {
+            frequencies.put(num, frequencies.getOrDefault(num, 0) + 1);
+        }
+
+        // Determine the maximum frequency
+        int maxFrequency = 0;
+        for (int frequency : frequencies.values()) {
+            maxFrequency = Math.max(maxFrequency, frequency);
+        }
+
+        // Calculate the total frequencies of elements with the maximum frequency
+        int frequencyOfMaxFrequency = 0;
+        for (int frequency : frequencies.values()) {
+            if (frequency == maxFrequency) {
+                frequencyOfMaxFrequency++;
+            }
+        }
+        return frequencyOfMaxFrequency * maxFrequency;
+    }
+}
