bbbb0459df17bff0ec685ef06cfc0c2657dbc19b
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-09-26 21:12:20 +0530

    Add submission 1783418617

---
+++ submissions/Valid Triangle Number/1783418617.java
@@ -0,0 +1,22 @@
+# Title: Valid Triangle Number
+# Submission ID: 1783418617
+# Status: Internal Error
+# Date: September 26, 2025 at 09:12:13 PM GMT+5:30
+
+class Solution {
+    public int triangleNumber(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] > nums[k] && 
+                        nums[i] + nums[k] > nums[j] && 
+                        nums[j] + nums[k] > nums[i]) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
