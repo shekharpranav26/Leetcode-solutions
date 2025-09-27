6824b46302cd8059de385c625caf153b5254c5c0
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-09-27 21:07:54 +0530

    Add submission 1784364268

---
+++ submissions/Largest Triangle Area/1784364268.java
@@ -0,0 +1,21 @@
+# Title: Largest Triangle Area
+# Submission ID: 1784364268
+# Status: Accepted
+# Date: September 27, 2025 at 09:07:48 PM GMT+5:30
+
+class Solution {
+    public double largestTriangleArea(int[][] points) {
+        int N = points.length;
+        double ans = 0;
+        for (int i = 0; i < N; ++i)
+            for (int j = i+1; j < N; ++j)
+                for (int k = j+1; k < N; ++k)
+                    ans = Math.max(ans, area(points[i], points[j], points[k]));
+        return ans;
+    }
+
+    public double area(int[] P, int[] Q, int[] R) {
+        return 0.5 * Math.abs(P[0]*Q[1] + Q[0]*R[1] + R[0]*P[1]
+                             -P[1]*Q[0] - Q[1]*R[0] - R[1]*P[0]);
+    }
+}
