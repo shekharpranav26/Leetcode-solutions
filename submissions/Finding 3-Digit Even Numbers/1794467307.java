0a28aa159da5f7668bf0e1ba94519276b4bf1482
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-10-07 22:49:57 +0530

    Add submission 1794467307

---
+++ submissions/Finding 3-Digit Even Numbers/1794467307.java
@@ -0,0 +1,35 @@
+# Title: Finding 3-Digit Even Numbers
+# Submission ID: 1794467307
+# Status: Accepted
+# Date: October 7, 2025 at 10:49:50 PM GMT+5:30
+
+public class Solution {
+
+    public int[] findEvenNumbers(int[] digits) {
+        Set<Integer> nums = new HashSet<>();
+        int n = digits.length;
+        // Traverse the indices of three digits
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                for (int k = 0; k < n; ++k) {
+                    // Determine whether it meets the condition of the target even number
+                    if (i == j || j == k || i == k) {
+                        continue;
+                    }
+                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                    if (num >= 100 && num % 2 == 0) {
+                        nums.add(num);
+                    }
+                }
+            }
+        }
+        // Converted to an array sorted in ascending order
+        List<Integer> res = new ArrayList<>(nums);
+        Collections.sort(res);
+        int[] result = new int[res.size()];
+        for (int i = 0; i < res.size(); ++i) {
+            result[i] = res.get(i);
+        }
+        return result;
+    }
+}
