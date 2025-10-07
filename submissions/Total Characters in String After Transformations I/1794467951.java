586c6ed0a3285d1e5d0039bb131d812d575b1a07
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-10-07 22:50:28 +0530

    Add submission 1794467951

---
+++ submissions/Total Characters in String After Transformations I/1794467951.java
@@ -0,0 +1,30 @@
+# Title: Total Characters in String After Transformations I
+# Submission ID: 1794467951
+# Status: Accepted
+# Date: October 7, 2025 at 10:50:22 PM GMT+5:30
+
+class Solution {
+
+    private static final int MOD = 1000000007;
+
+    public int lengthAfterTransformations(String s, int t) {
+        int[] cnt = new int[26];
+        for (char ch : s.toCharArray()) {
+            ++cnt[ch - 'a'];
+        }
+        for (int round = 0; round < t; ++round) {
+            int[] nxt = new int[26];
+            nxt[0] = cnt[25];
+            nxt[1] = (cnt[25] + cnt[0]) % MOD;
+            for (int i = 2; i < 26; ++i) {
+                nxt[i] = cnt[i - 1];
+            }
+            cnt = nxt;
+        }
+        int ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            ans = (ans + cnt[i]) % MOD;
+        }
+        return ans;
+    }
+}
