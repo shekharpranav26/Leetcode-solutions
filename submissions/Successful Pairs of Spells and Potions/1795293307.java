d9ba77e83b2d6320f6296095d5915a3519209f28
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-10-08 18:18:13 +0530

    Add submission 1795293307

---
+++ submissions/Successful Pairs of Spells and Potions/1795293307.java
@@ -0,0 +1,33 @@
+# Title: Successful Pairs of Spells and Potions
+# Submission ID: 1795293307
+# Status: Accepted
+# Date: October 8, 2025 at 06:18:06 PM GMT+5:30
+
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int n = spells.length;
+        int ans[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int idx = bs(potions, spells[i], success);
+            if (idx != -1) ans[i] = potions.length - idx;
+        }
+
+        return ans;
+    }
+
+    int bs(int potions[], long strength, long success) {
+        int low = 0, high = potions.length - 1, idx = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if ((long) potions[mid] * strength >= success) {
+                idx = mid;
+                high = mid - 1; // find smaller index
+            } else {
+                low = mid + 1;
+            }
+        }
+        return idx;
+    }
+}
