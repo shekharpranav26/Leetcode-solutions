c1b83b5a3844d2dd52a0a29643b38730711f1d11
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-10-10 00:37:17 +0530

    Add submission 1796700837

---
+++ submissions/Find the Minimum Amount of Time to Brew Potions/1796700837.java
@@ -0,0 +1,26 @@
+# Title: Find the Minimum Amount of Time to Brew Potions
+# Submission ID: 1796700837
+# Status: Accepted
+# Date: October 10, 2025 at 12:37:10 AM GMT+5:30
+
+class Solution {
+
+    public long minTime(int[] skill, int[] mana) {
+        int n = skill.length;
+        int m = mana.length;
+        long[] times = new long[n];
+
+        for (int j = 0; j < m; j++) {
+            long curTime = 0;
+            for (int i = 0; i < n; i++) {
+                curTime =
+                    Math.max(curTime, times[i]) + (long) skill[i] * mana[j];
+            }
+            times[n - 1] = curTime;
+            for (int i = n - 2; i >= 0; i--) {
+                times[i] = times[i + 1] - (long) skill[i + 1] * mana[j];
+            }
+        }
+        return times[n - 1];
+    }
+}
