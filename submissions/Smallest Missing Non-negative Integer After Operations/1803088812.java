9576be0c0e2f2e1f8ac4402014fb2a601881dfa5
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-10-16 11:35:09 +0530

    Add submission 1803088812

---
+++ submissions/Smallest Missing Non-negative Integer After Operations/1803088812.java
@@ -0,0 +1,21 @@
+# Title: Smallest Missing Non-negative Integer After Operations
+# Submission ID: 1803088812
+# Status: Accepted
+# Date: October 16, 2025 at 11:35:02 AM GMT+5:30
+
+class Solution {
+
+    public int findSmallestInteger(int[] nums, int value) {
+        int[] mp = new int[value];
+        for (int x : nums) {
+            int v = ((x % value) + value) % value;
+            mp[v]++;
+        }
+        int mex = 0;
+        while (mp[mex % value] > 0) {
+            mp[mex % value]--;
+            mex++;
+        }
+        return mex;
+    }
+}
