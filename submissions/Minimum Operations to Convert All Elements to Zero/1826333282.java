18ec6a57d2eda319051def4990e20037897100f3
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-11-11 01:41:50 +0530

    Add submission 1826333282

---
+++ submissions/Minimum Operations to Convert All Elements to Zero/1826333282.java
@@ -0,0 +1,23 @@
+# Title: Minimum Operations to Convert All Elements to Zero
+# Submission ID: 1826333282
+# Status: Accepted
+# Date: November 11, 2025 at 01:41:43 AM GMT+5:30
+
+class Solution {
+
+    public int minOperations(int[] nums) {
+        List<Integer> s = new ArrayList<>();
+        int res = 0;
+        for (int a : nums) {
+            while (!s.isEmpty() && s.get(s.size() - 1) > a) {
+                s.remove(s.size() - 1);
+            }
+            if (a == 0) continue;
+            if (s.isEmpty() || s.get(s.size() - 1) < a) {
+                res++;
+                s.add(a);
+            }
+        }
+        return res;
+    }
+}
