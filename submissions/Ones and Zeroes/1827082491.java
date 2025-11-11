307aa4311a6c6e2f381c72bb98a66c452aee0cbc
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-11-11 22:16:58 +0530

    Add submission 1827082491

---
+++ submissions/Ones and Zeroes/1827082491.java
@@ -0,0 +1,25 @@
+# Title: Ones and Zeroes
+# Submission ID: 1827082491
+# Status: Accepted
+# Date: November 11, 2025 at 10:16:51 PM GMT+5:30
+
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (String s : strs) {
+            int z = 0, o = 0;
+            for (char c : s.toCharArray()) {
+                if (c == '0') z++;
+                else o++;
+            }
+
+            for (int i = m; i >= z; i--) {
+                for (int j = n; j >= o; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - z][j - o] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
