fd2c60a424b2b69965e71dcc244c9e956fd48d72
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-11-17 15:50:25 +0530

    Add submission 1832188515

---
+++ submissions/Check If All 1's Are at Least Length K Places Away/1832188515.java
@@ -0,0 +1,22 @@
+# Title: Check If All 1's Are at Least Length K Places Away
+# Submission ID: 1832188515
+# Status: Accepted
+# Date: November 17, 2025 at 03:50:18 PM GMT+5:30
+
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        int lastIndex = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 1) {
+                if (lastIndex != -1 && i - lastIndex - 1 < k) {
+                    return false;
+                }
+                lastIndex = i;
+            }
+        }
+
+        return true;
+    }
+}
