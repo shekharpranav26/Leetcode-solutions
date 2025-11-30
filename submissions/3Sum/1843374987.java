1f15425317651f73350cef53cca0017046085ce9
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-11-30 19:14:38 +0530

    Add submission 1843374987

---
+++ submissions/3Sum/1843374987.java
@@ -0,0 +1,31 @@
+# Title: 3Sum
+# Submission ID: 1843374987
+# Status: Accepted
+# Date: November 30, 2025 at 07:14:30 PM GMT+5:30
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>>ans=new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    j++;
+                }else if(sum>0){
+                    k--;
+                }else{
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
+                }
+            }
+        }
+        return ans;
+    }
+}
