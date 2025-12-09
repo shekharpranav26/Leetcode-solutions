bce177e297a9a9b9a8ebbe7c557bccb674093333
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-12-09 23:52:14 +0530

    Add submission 1851378859

---
+++ submissions/Merge Two Sorted Lists/1851378859.java
@@ -0,0 +1,26 @@
+# Title: Merge Two Sorted Lists
+# Submission ID: 1851378859
+# Status: Accepted
+# Date: December 9, 2025 at 11:52:08 PM GMT+5:30
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val > list2.val) {
+                cur.next = list2;
+                list2 = list2.next;
+            } else {
+                cur.next = list1;
+                list1 = list1.next;
+            }
+            cur = cur.next;
+        }
+
+        cur.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next;        
+    }
+}
