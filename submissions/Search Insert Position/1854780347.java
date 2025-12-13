0f359ab1e1abeb250e9430f52d9496f7d40506f9
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-12-14 00:46:14 +0530

    Add submission 1854780347

---
+++ submissions/Search Insert Position/1854780347.java
@@ -0,0 +1,25 @@
+# Title: Search Insert Position
+# Submission ID: 1854780347
+# Status: Accepted
+# Date: December 14, 2025 at 12:46:01 AM GMT+5:30
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;        
+    }
+}
