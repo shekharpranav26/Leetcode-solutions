d5440da546b0996d858367ec0ac1fb2eb290b457
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2025-12-18 21:15:25 +0530

    Add submission 1859049275

---
+++ submissions/Climbing Stairs/1859049275.java
@@ -0,0 +1,21 @@
+# Title: Climbing Stairs
+# Submission ID: 1859049275
+# Status: Accepted
+# Date: December 18, 2025 at 09:14:31 PM GMT+5:30
+
+class Solution {
+    public int climbStairs(int n) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        return climbStairs(n, memo);
+    }
+    
+    private int climbStairs(int n, Map<Integer, Integer> memo) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (!memo.containsKey(n)) {
+            memo.put(n, climbStairs(n-1, memo) + climbStairs(n-2, memo));
+        }
+        return memo.get(n);
+    }
+}
