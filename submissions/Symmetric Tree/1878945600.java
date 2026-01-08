c5bc5ee956b39461cbd2d30eee1dee88f648c898
Pranav Shekhar <134296362+shekharpranav26@users.noreply.github.com>
2026-01-08 22:17:42 +0530

    Add submission 1878945600

---
+++ submissions/Symmetric Tree/1878945600.java
@@ -0,0 +1,40 @@
+# Title: Symmetric Tree
+# Submission ID: 1878945600
+# Status: Accepted
+# Date: January 8, 2026 at 10:17:35 PM GMT+5:30
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        return find_sym(root.left,root.right);
+    }
+    static boolean find_sym(TreeNode left,TreeNode right){
+        if(left==null&&right==null){
+            return true;
+        }
+        if(left==null||right==null){
+            return false;
+        }
+        if(left.val!=right.val){
+            return false;
+        }
+        return find_sym(left.left,right.right)&&find_sym(left.right,right.left);
+    }
+}
